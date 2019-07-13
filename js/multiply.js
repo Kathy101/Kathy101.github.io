2c3d43a84a9a419d9a3ddc558711c9d57c7a0b1f
Kathy <mktz0105@gmail.com>
2019-07-13 00:21:12 -0400

    add some comments, others can understand my code

--- js/multiply.js
+++ js/multiply.js
@@ -2,18 +2,21 @@
 var ranNum1, ranNum2, ranMult0, AnsGood, SolutionZ;
 
 function RndA0() {
+ //Generate a random number for multiplication test.
     var x = document.getElementById("Rnd0");
     ranNum1 = Math.floor((Math.random() * 100) + 1);
     x.innerHTML=ranNum1;
 }
 
 function RndB0() {
+ //Generate a random number for multiplication test.
     var x = document.getElementById("Rnd1");
     ranNum2 = Math.floor((Math.random() * 100) + 1);
     x.innerHTML=ranNum2;
 }
 
 function multiply() {
+    //Generate the true result for compare to use input.
     ranMult0 = ranNum1 * ranNum2;
     console.log('ranNum1 x ranNum2==>', ranNum1 * ranNum2);
     return(ranMult0);
@@ -21,6 +24,7 @@ function multiply() {
 }
 
 function ResultChk() {
+ //Compare the results to see if the user get it right or wrong.
  var answer0 = document.getElementById("myNumber").value;
  console.log('answer0 * 1==>', answer0 * 1);
  AnsGood = 0;
